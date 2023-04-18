2a278f423cc8464b7d95e2a844d28106db2f0193
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-18 18:05:09 -0500

    User Story 2~

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,4 +2,17 @@ class DishesController < ApplicationController
   def show
     @dish = Dish.find(params[:id])
   end
+  
+  def update
+    @dish = Dish.find(params[:id])
+    @dish.update(dishes_params)
+
+    redirect_to dish_path(@dish)
+  end
+
+  private
+
+  def dishes_params
+    params.permit(:id)
+  end
 end
--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe 'Dish show page' do
   let!(:dish_1) { gorden.dishes.create!(name: 'Tofu Stirfry', description: "Tofu and veggies wok tossed and lightly seasoned with soy sauce" ) }
   let!(:ingredient_1) { Ingredient.create!(name: 'Tofu', calories: "50" ) }
   let!(:ingredient_2) { Ingredient.create!(name: 'Bell Pepper', calories: "20" ) }
+  let!(:ingredient_3) { Ingredient.create!(name: 'Onion', calories: "15" ) }
   let!(:dish_ingredients_1) { DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_1.id) }
   let!(:dish_ingredients_2) { DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_2.id) }
+  let!(:dish_ingredients_3) { DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_3.id) }
 
   describe 'Dishes attributes and chef who made it' do
     it 'should display the dishes name and description' do
@@ -17,7 +19,7 @@ RSpec.describe 'Dish show page' do
     end
     it 'should display the list of ingredients for the dishes and a total calorie count' do
       visit dish_path(dish_1)
-      save_and_open_page
+      
       expect(page).to have_content(ingredient_1.name)
       expect(page).to have_content(ingredient_1.calories.to_i)
       expect(page).to have_content(ingredient_2.calories.to_i)
@@ -26,6 +28,21 @@ RSpec.describe 'Dish show page' do
       visit dish_path(dish_1)
 
       expect(page).to have_content("Gorden Ramsey")
+    end
+  end
+
+  describe 'Form to add ingredients' do
+    it 'should display a form to add exisiting ingredients' do
+      visit dish_path(dish_1)
+      save_and_open_page
+      expect(page).to have_button("Add My Ingredient")
+      expect(page).to have_field(:id)
+      
+      fill_in :id, with: 3
+      click_button "Add My Ingredient"
+
+      expect(current_path).to eq(visit dish_path(dish_1))
+      expect(page).to have_content(ingredient_3.name)
     end
   end
 end
