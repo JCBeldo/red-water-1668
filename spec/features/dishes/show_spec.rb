c77e46a579aafdaaf539180070428a5936f0813d
Julian Beldotti <julian.beldotti@gmail.com>
2023-04-18 17:46:05 -0500

    User story 1

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -7,19 +7,25 @@ RSpec.describe 'Dish show page' do
   let!(:ingredient_2) { Ingredient.create!(name: 'Bell Pepper', calories: "20" ) }
   let!(:dish_ingredients_1) { DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_1.id) }
   let!(:dish_ingredients_2) { DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_2.id) }
-  
+
   describe 'Dishes attributes and chef who made it' do
     it 'should display the dishes name and description' do
       visit dish_path(dish_1)
+
       expect(page).to have_content(dish_1.name)
       expect(page).to have_content(dish_1.description)
     end
     it 'should display the list of ingredients for the dishes and a total calorie count' do
       visit dish_path(dish_1)
-      
+      save_and_open_page
+      expect(page).to have_content(ingredient_1.name)
+      expect(page).to have_content(ingredient_1.calories.to_i)
+      expect(page).to have_content(ingredient_2.calories.to_i)
     end
     it 'should display the name of the chef who made the dish' do
       visit dish_path(dish_1)
+
+      expect(page).to have_content("Gorden Ramsey")
     end
   end
 end
